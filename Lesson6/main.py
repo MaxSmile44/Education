f2453ace0bcbf9a1c6cfcb59483a8fea315edef2
MaxSmile44 <afmaxim89@gmail.com>
2024-11-26 13:00:45 +0300

    Add Lesson7

--- Lesson6/main.py
+++ Lesson6/main.py
@@ -1,28 +1,53 @@
+import urwid
+
+
 def is_very_long(password):
-    return True if len(password) > 12 else False
+    return len(password) > 12
+
 
 def has_digit(password):
     return any(letter.isdigit() for letter in password)
 
+
 def has_letters(password):
     return any(letter.isalpha() for letter in password)
+
 
 def has_upper_letters(password):
     return any(letter.isupper() for letter in password)
 
+
 def has_lower_letters(password):
     return any(letter.islower() for letter in password)
+
 
 def has_symbols(password):
     return any(not letter.isalnum() for letter in password)
 
-def rating_password(password):
+
+def rating_password(edit, password):
     score = 0
-    for function in [is_very_long, has_digit, has_letters, has_upper_letters, has_lower_letters, has_symbols]:
+    for function in [
+        is_very_long,
+        has_digit,
+        has_letters,
+        has_upper_letters,
+        has_lower_letters,
+        has_symbols
+    ]:
         score += 2 if function(password) else 0
-    return score
+    reply.set_text("Рейтинг пароля: %s" % score)
+
+
+def main():
+    global reply
+    ask = urwid.Edit('Введите пароль: ', mask='*')
+    reply = urwid.Text("")
+    menu = urwid.Pile([ask, reply])
+    menu = urwid.Filler(menu, valign='top')
+    urwid.connect_signal(ask, 'change', rating_password)
+    urwid.MainLoop(menu).run()
 
 
 if __name__ == '__main__':
-    password = input('Введите пароль: ')
-    print(f'Рейтинг пароля: {rating_password(password)}')
+    main()
---
+++ Lesson7/main.py
@@ -0,0 +1,39 @@
+import os
+import ptbot
+from dotenv import load_dotenv
+from pytimeparse import parse
+
+
+def reply(chat_id, question):
+    message_id = bot.send_message(chat_id, "Запускаю таймер")
+    message_id_bar = bot.send_message(chat_id, render_progressbar(parse(question), 0))
+    bot.create_countdown(parse(question), notify_progress, chat_id=chat_id, message_id=message_id, message_id_bar=message_id_bar, question=question)
+    bot.create_timer(parse(question), notify, chat_id=chat_id)
+
+
+def notify_progress(secs_left, chat_id, message_id, message_id_bar, question):
+    bot.update_message(chat_id, message_id, f"Осталось секунд: {secs_left}")
+    bot.update_message(chat_id, message_id_bar, render_progressbar(parse(question), parse(question) - secs_left))
+
+
+def notify(chat_id):
+    bot.send_message(chat_id, "Время вышло!")
+
+
+def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
+    iteration = min(total, iteration)
+    percent = "{0:.1f}"
+    percent = percent.format(100 * (iteration / float(total)))
+    filled_length = int(length * iteration // total)
+    pbar = fill * filled_length + zfill * (length - filled_length)
+    return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    TG_TOKEN = os.getenv("TELEGRAM_TOKEN")
+    bot = ptbot.Bot(TG_TOKEN)
+
+    bot.reply_on_message(reply)
+
+    bot.run_bot()
