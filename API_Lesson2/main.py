723f504ae65719c5e471df343e4dc9b82218d819
MaxSmile44 <afmaxim89@gmail.com>
2024-12-18 06:49:37 +0300

    Update

--- API_Lesson2/main.py
+++ API_Lesson2/main.py
@@ -8,40 +8,57 @@ import requests
 def shorten_link(token, url):
     vk_url = 'https://api.vk.ru/method/utils.getShortLink'
     payload = {'v': '5.81', 'access_token': token, 'url': url}
-    responce = requests.get(vk_url, params=payload)
-    return responce.json()['response']['short_url']
+    response = requests.get(vk_url, params=payload)
+    try:
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as error:
+        print(f'HTTP error occurred: {error}')
+    decoded_response = response.json()
+    if 'error' in decoded_response:
+        raise requests.exceptions.HTTPError(decoded_response['error'])
+    return response.json()['response']['short_url']
 
 
 def count_clicks(token, url):
     vk_url = 'https://api.vk.ru/method/utils.getLinkStats'
+    url = urlparse(url).path.replace('/', '').replace('vk.cc', '')
     payload = {'v': '5.81', 'access_token': token, 'key': url}
-    responce = requests.get(vk_url, params=payload)
-    return responce.json()
-
-
-def is_shorten_link(url):
-    return urlparse(url).netloc == 'vk.cc'
-
-
-def main(token):
-    url = input('Введите ссылку: ')
-    url = 'https://' + url if not url.startswith('http') else url
+    response = requests.get(vk_url, params=payload)
     try:
-        check_link = requests.get(url)
-        if check_link.status_code == 200:
-            if not is_shorten_link(url):
-                short_link = shorten_link(token=token, url=url)
-                print(f'Сокращенная ссылка: {short_link}')
-            else:
-                print(f'Количество переходов по ссылке: '
-                      f'{count_clicks(token, urlparse(url).path[1:])['response']['stats'][0]['views']}')
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as error:
+        print(f'HTTP error occurred: {error}')
+    decoded_response = response.json()
+    if 'error' in decoded_response:
+        raise requests.exceptions.HTTPError(decoded_response['error'])
+    return response.json()['response']['stats'][0]['views']
+
+
+def is_shorten_link(token, url):
+    try:
+        count_clicks(token, url)
+        return True
+    except requests.exceptions.HTTPError:
+        return False
+
+
+def main():
+    url = input('Введите ссылку: ')
+    try:
+        load_dotenv()
+        token = os.environ["VK_TOKEN"]
+    except KeyError as error:
+        print(f'KeyError: {error}')
+        raise SystemExit
+    try:
+        if is_shorten_link(token, url):
+            print(f'Количество переходов: {count_clicks(token, url)}')
         else:
-            print('Такого адреса не существует')
-    except (requests.exceptions.ConnectionError, requests.exceptions.MissingSchema, requests.exceptions.InvalidSchema):
-        print('Такого адреса не существует')
+            print(f'Сокращенная ссылка: {shorten_link(token, url)}')
+    except requests.exceptions.HTTPError as error:
+        print(f'HTTP error occurred: {error}')
+        raise SystemExit
 
 
 if __name__ == '__main__':
-    load_dotenv()
-    vk_token = os.getenv("VK_TOKEN")
-    main(vk_token)
+    main()
